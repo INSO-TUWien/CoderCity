659a5e6bb71e75004fffa8324d7e869dadcba976
Yikai Yang <e1427424@student.tuwien.ac.at>
2020-02-13 18:18:48 +0100

    Added sync scrolling implementation via services

--- frontend/src/app/timeline/gitgraph/commit-message-container/commit-message-container.component.ts
+++ frontend/src/app/timeline/gitgraph/commit-message-container/commit-message-container.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { State } from 'src/app/reducers';
 import * as GitActions from '../../gitgraph/git.action';
 import { map, tap } from 'rxjs/operators';
 import { Commit } from 'src/app/shared/git/commit.model';
+import { GitgraphService } from '../gitgraph.service';
 
 @Component({
   selector: 'cc-commit-message-container',
@@ -13,9 +14,18 @@ import { Commit } from 'src/app/shared/git/commit.model';
 })
 export class CommitMessageContainerComponent implements OnInit {
 
+  @ViewChild('messageContainer', {static: true})
+  messageContainer: ElementRef<HTMLElement>;
+
+  // Element is active when mouse is hovering above it.
+  active: boolean;
+
   commits$: Observable<Commit[]>;
 
-  constructor(private store: Store<State>) {
+  constructor(
+    private store: Store<State>,
+    private gitGraphService: GitgraphService
+  ) {
     this.commits$ = store.pipe(
         select(store => store.git.commits),
           map(val => [...val]),
@@ -28,6 +38,38 @@ export class CommitMessageContainerComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initEvents();
+
+    this.gitGraphService.scrollLeft.subscribe(value => {
+      if (!this.active) {
+        this.setScrollLeft(value);
+      }
+    });
+  }
+
+  private initEvents(): void {
+    this.messageContainer.nativeElement.onmouseenter = () => {
+      this.active = true;
+    };
+
+    this.messageContainer.nativeElement.onmouseleave = () => {
+      this.active = false;
+    };
+
+    this.messageContainer.nativeElement.onscroll = () => {
+      if (this.active) {
+        const scrollLeft = this.messageContainer.nativeElement.scrollLeft;
+        this.scroll(scrollLeft);
+      }
+    };
+  }
+
+  private setScrollLeft(value: number) {
+    this.messageContainer.nativeElement.scrollLeft = value;
+  }
+
+  private scroll(value: number) {
+    this.gitGraphService.scrollLeft.next(value);
   }
 
   onCommitsClick() {
