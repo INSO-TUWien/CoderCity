162a8aaa2bb33306c9cba3277dd63098dbac2a7b
Yikai Yang <yikai.yang@ymail.com>
2020-07-10 22:32:29 +0200

    Removed uncessaray files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GitGateway } from './socket/git.gateway';
 import { GitService } from './services/git/git.service';
 import { CommitController } from './controller/commit.controller';
 import { CommitService } from './services/commit/commit.service';
@@ -15,6 +13,6 @@ import { AuthorService } from './services/author/author.service';
     envFilePath: ['.env.development', '.env'],
   })],
   controllers: [AppController, CommitController, ProjectController],
-  providers: [AppService, GitService, AuthorService, CommitService, ProjectService],
+  providers: [GitService, AuthorService, CommitService, ProjectService],
 })
 export class AppModule {}
