f0656aadd2da5e9e8f12ced4a6f6a6b5fb09f3ed
Yikai Yang <yikai.yang@ymail.com>
2021-01-03 10:34:32 +0000

    Revert "Merge branch 'master' into 10-fix-branch-labels"
    This reverts commit 563811da3ed860a072ae4131d2b88f483283f329

--- frontend/src/app/components/timeline/gitgraph/branch-tag/branch-tag.component.spec.ts
+++ frontend/src/app/components/timeline/gitgraph/branch-tag/branch-tag.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<< HEAD:frontend/src/app/components/timeline/gitgraph/branch-tag/branch-tag.component.spec.ts
 import { BranchTagComponent } from './branch-tag.component';
 
 describe('BranchTagComponent', () => {
@@ -10,27 +9,12 @@ describe('BranchTagComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ BranchTagComponent ]
-=======
-import { TooltipComponent } from './tooltip.component';
-
-describe('TooltipComponent', () => {
-  let component: TooltipComponent;
-  let fixture: ComponentFixture<TooltipComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TooltipComponent ]
->>>>>>> master:frontend/src/app/components/timeline/gitgraph/tooltip/tooltip.component.spec.ts
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-<<<<<<< HEAD:frontend/src/app/components/timeline/gitgraph/branch-tag/branch-tag.component.spec.ts
     fixture = TestBed.createComponent(BranchTagComponent);
-=======
-    fixture = TestBed.createComponent(TooltipComponent);
->>>>>>> master:frontend/src/app/components/timeline/gitgraph/tooltip/tooltip.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
